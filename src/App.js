3c839cab1aa7221e4db078a5aafd425d38dc9701
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-08-06 10:35:39 -0400

    ui

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const average = (arr) =>
 const KEY = "f84fc31d";
 
 export default function App() {
-  const [query, setQuery] = useState("Inception");
+  const [query, setQuery] = useState("Deadpool");
   const [selectedId, setSelectedId] = useState(null);
   const { movies, isLoading, error } = useMovies(query);
 
